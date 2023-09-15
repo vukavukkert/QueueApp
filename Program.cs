47b795a22b0c00b7a62fddecdc0f73c2a8e10519
user <user@VUKKERT>
2023-09-15 13:28:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using QueueApp;
+Queue queue = new Queue(10);
+for (int i = 0; i < queue.Length; i++)
+{
+    queue.Enqueue(i + 1);
+    Print(queue.Peek());
+}
+static void Print(int a)
+{
+    Console.WriteLine(a);
+}
---
+++ Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueApp
+{
+    internal class Queue
+    {
+        List<int> newList;
+        public int Length;
+        private int Count = 0;
+        public object Clone() => MemberwiseClone();
+        public Queue(int Length)
+        {
+            newList = new List<int>(Length);
+            this.Length = Length;
+        }
+        public Queue(Queue obj)
+        {
+            Length = obj.Length;
+            newList = obj.newList;
+            Count = obj.Count;
+        }
+        public void Enqueue(int value)
+        {
+            if (Count < Length)
+            {
+                newList.Add(value);
+                Count++;
+            }
+            else
+            {
+                throw new InvalidOperationException("Queue is full");
+            }
+        }
+        public int Dequeue()
+        {
+            Count--;
+            int a = newList[Count];
+            newList.RemoveAt(Count);
+            return a;
+        }
+        public int Peek()
+        {
+            return newList[0];
+        }
+        public void Clear()
+        {
+            newList = new List<int>(Length);
+            Count = 0;
+        }
+        public bool Contains(int i)
+        {
+            if (Count == 0) return false;
+            foreach (var item in newList) { if (item == i) return true;}
+            return false;
+        }
+    }
+}
